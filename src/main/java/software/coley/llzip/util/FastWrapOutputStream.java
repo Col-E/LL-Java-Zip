f38bd945bd4646addf3c764ca6990dc60b203a1f
Col-E <mcoley2@gmu.edu>
2023-06-13 21:44:26 -0400

    Document FastWrapOutputStream

--- src/main/java/software/coley/llzip/util/FastWrapOutputStream.java
+++ src/main/java/software/coley/llzip/util/FastWrapOutputStream.java
@@ -2,7 +2,15 @@ package software.coley.llzip.util;
 
 import java.io.ByteArrayOutputStream;
 
+/**
+ * Byte output stream with {@link BufferData} wrapping provided, without the array-copy of {@link #toByteArray()}.
+ *
+ * @author xDark
+ */
 public final class FastWrapOutputStream extends ByteArrayOutputStream {
+	/**
+	 * @return Wrapper of the current buffer.
+	 */
 	public BufferData wrap() {
 		return BufferData.wrap(buf, 0, count);
 	}
