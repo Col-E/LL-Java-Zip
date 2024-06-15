88f9ba7d91a3c825be929ac3460a4d5abb1659cc
Matt <mcoley2@gmu.edu>
2024-06-15 04:20:10 -0400

    Pull out writing portions of the archive in DirectZipWriter to protected methods

--- src/main/java/software/coley/lljzip/format/write/DirectZipWriter.java
+++ src/main/java/software/coley/lljzip/format/write/DirectZipWriter.java
@@ -21,68 +21,77 @@ public class DirectZipWriter implements ZipWriter {
 	@Override
 	public void write(@Nonnull ZipArchive archive, @Nonnull OutputStream os) throws IOException {
 		// Write local file headers.
-		for (final LocalFileHeader fileHeader : archive.getLocalFiles()) {
-			writeIntLE(os, ZipPatterns.LOCAL_FILE_HEADER_QUAD);
-			writeShortLE(os, fileHeader.getVersionNeededToExtract());
-			writeShortLE(os, fileHeader.getGeneralPurposeBitFlag());
-			writeShortLE(os, fileHeader.getCompressionMethod());
-			writeShortLE(os, fileHeader.getLastModFileTime());
-			writeShortLE(os, fileHeader.getLastModFileDate());
-			writeIntLE(os, fileHeader.getCrc32());
-			writeIntLE(os, (int) fileHeader.getCompressedSize());
-			writeIntLE(os, (int) fileHeader.getUncompressedSize());
-			writeShortLE(os, fileHeader.getFileNameLength());
-			writeShortLE(os, fileHeader.getExtraFieldLength());
-			os.write(MemorySegmentUtil.toByteArray(fileHeader.getFileName()));
-			os.write(MemorySegmentUtil.toByteArray(fileHeader.getExtraField()));
-			os.write(MemorySegmentUtil.toByteArray(fileHeader.getFileData()));
-		}
+		for (final LocalFileHeader fileHeader : archive.getLocalFiles())
+			writeLocalFile(fileHeader, os);
 
 		// Write central directory file headers.
-		for (final CentralDirectoryFileHeader directory : archive.getCentralDirectories()) {
-			writeIntLE(os, ZipPatterns.CENTRAL_DIRECTORY_FILE_HEADER_QUAD);
-			writeShortLE(os, directory.getVersionMadeBy());
-			writeShortLE(os, directory.getVersionNeededToExtract());
-			writeShortLE(os, directory.getGeneralPurposeBitFlag());
-			writeShortLE(os, directory.getCompressionMethod());
-			writeShortLE(os, directory.getLastModFileTime());
-			writeShortLE(os, directory.getLastModFileDate());
-			writeIntLE(os, directory.getCrc32());
-			writeIntLE(os, (int) directory.getCompressedSize());
-			writeIntLE(os, (int) directory.getUncompressedSize());
-			writeShortLE(os, directory.getFileNameLength());
-			writeShortLE(os, directory.getExtraFieldLength());
-			writeShortLE(os, directory.getFileCommentLength());
-			writeShortLE(os, directory.getDiskNumberStart());
-			writeShortLE(os, directory.getInternalFileAttributes());
-			writeIntLE(os, directory.getExternalFileAttributes());
-			writeIntLE(os, (int) directory.getRelativeOffsetOfLocalHeader());
-			os.write(MemorySegmentUtil.toByteArray(directory.getFileName()));
-			os.write(MemorySegmentUtil.toByteArray(directory.getExtraField()));
-			os.write(MemorySegmentUtil.toByteArray(directory.getFileComment()));
-		}
+		for (final CentralDirectoryFileHeader directory : archive.getCentralDirectories())
+			writeCentralDirectory(directory, os);
 
 		// Write end of central directory record.
 		final EndOfCentralDirectory end = archive.getEnd();
-		if (end != null) {
-			writeIntLE(os, ZipPatterns.END_OF_CENTRAL_DIRECTORY_QUAD);
-			writeShortLE(os, end.getDiskNumber());
-			writeShortLE(os, end.getCentralDirectoryStartDisk());
-			writeShortLE(os, end.getCentralDirectoryStartOffset());
-			writeShortLE(os, end.getNumEntries());
-			writeIntLE(os, (int) end.getCentralDirectorySize());
-			writeIntLE(os, (int) end.getCentralDirectoryOffset());
-			writeShortLE(os, end.getZipCommentLength());
-			os.write(MemorySegmentUtil.toByteArray(end.getZipComment()));
-		}
+		if (end != null)
+			writeEnd(end, os);
 	}
 
-	private static void writeShortLE(OutputStream os, int value) throws IOException {
+	protected void writeLocalFile(@Nonnull LocalFileHeader fileHeader, @Nonnull OutputStream os) throws IOException {
+		writeIntLE(os, ZipPatterns.LOCAL_FILE_HEADER_QUAD);
+		writeShortLE(os, fileHeader.getVersionNeededToExtract());
+		writeShortLE(os, fileHeader.getGeneralPurposeBitFlag());
+		writeShortLE(os, fileHeader.getCompressionMethod());
+		writeShortLE(os, fileHeader.getLastModFileTime());
+		writeShortLE(os, fileHeader.getLastModFileDate());
+		writeIntLE(os, fileHeader.getCrc32());
+		writeIntLE(os, (int) fileHeader.getCompressedSize());
+		writeIntLE(os, (int) fileHeader.getUncompressedSize());
+		writeShortLE(os, fileHeader.getFileNameLength());
+		writeShortLE(os, fileHeader.getExtraFieldLength());
+		os.write(MemorySegmentUtil.toByteArray(fileHeader.getFileName()));
+		os.write(MemorySegmentUtil.toByteArray(fileHeader.getExtraField()));
+		os.write(MemorySegmentUtil.toByteArray(fileHeader.getFileData()));
+	}
+
+	protected void writeCentralDirectory(@Nonnull CentralDirectoryFileHeader directory, @Nonnull OutputStream os) throws IOException {
+		writeIntLE(os, ZipPatterns.CENTRAL_DIRECTORY_FILE_HEADER_QUAD);
+		writeShortLE(os, directory.getVersionMadeBy());
+		writeShortLE(os, directory.getVersionNeededToExtract());
+		writeShortLE(os, directory.getGeneralPurposeBitFlag());
+		writeShortLE(os, directory.getCompressionMethod());
+		writeShortLE(os, directory.getLastModFileTime());
+		writeShortLE(os, directory.getLastModFileDate());
+		writeIntLE(os, directory.getCrc32());
+		writeIntLE(os, (int) directory.getCompressedSize());
+		writeIntLE(os, (int) directory.getUncompressedSize());
+		writeShortLE(os, directory.getFileNameLength());
+		writeShortLE(os, directory.getExtraFieldLength());
+		writeShortLE(os, directory.getFileCommentLength());
+		writeShortLE(os, directory.getDiskNumberStart());
+		writeShortLE(os, directory.getInternalFileAttributes());
+		writeIntLE(os, directory.getExternalFileAttributes());
+		writeIntLE(os, (int) directory.getRelativeOffsetOfLocalHeader());
+		os.write(MemorySegmentUtil.toByteArray(directory.getFileName()));
+		os.write(MemorySegmentUtil.toByteArray(directory.getExtraField()));
+		os.write(MemorySegmentUtil.toByteArray(directory.getFileComment()));
+	}
+
+	protected void writeEnd(@Nonnull EndOfCentralDirectory end, @Nonnull OutputStream os) throws IOException {
+		writeIntLE(os, ZipPatterns.END_OF_CENTRAL_DIRECTORY_QUAD);
+		writeShortLE(os, end.getDiskNumber());
+		writeShortLE(os, end.getCentralDirectoryStartDisk());
+		writeShortLE(os, end.getCentralDirectoryStartOffset());
+		writeShortLE(os, end.getNumEntries());
+		writeIntLE(os, (int) end.getCentralDirectorySize());
+		writeIntLE(os, (int) end.getCentralDirectoryOffset());
+		writeShortLE(os, end.getZipCommentLength());
+		os.write(MemorySegmentUtil.toByteArray(end.getZipComment()));
+	}
+
+	protected static void writeShortLE(OutputStream os, int value) throws IOException {
 		os.write(value & 0xFF);
 		os.write((value >> 8) & 0xFF);
 	}
 
-	private static void writeIntLE(OutputStream os, int value) throws IOException {
+	protected static void writeIntLE(OutputStream os, int value) throws IOException {
 		os.write(value & 0xFF);
 		os.write((value >> 8) & 0xFF);
 		os.write((value >> 16) & 0xFF);
